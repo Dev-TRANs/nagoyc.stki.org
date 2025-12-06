2f66af05cf01fad198a9c481892f1868888127b2
Tsuyukusa Akari <akkun11.open@gmail.com>
2025-12-07 00:10:10 +0900

    csを中央にしてみる

--- src/lib/components/ComingSoon.svelte
+++ src/lib/components/ComingSoon.svelte
@@ -1,1 +1,13 @@
-<h1 class='lang-en'>Coming Soon...</h1>
+<style>
+.box {
+  display: flex;
+}
+
+h1 {
+  margin: auto;
+}
+</style>
+
+<div class='box'>
+  <h1 class='lang-en'>Coming Soon...</h1>
+</div>
